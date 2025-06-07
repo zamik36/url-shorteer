bb899edf45e992dfd7b4111910c42c48b56011a6
Ilya <ilyxaz68@mail.ru>
2025-06-08 00:35:55 +0300

    fixed bugs

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,11 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     TEST_DATABASE_URL: str
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
 
 
 settings = Settings()
